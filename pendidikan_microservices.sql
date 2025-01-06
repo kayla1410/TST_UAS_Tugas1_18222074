6f0333633e7bdd7702e24f60c9c21f1d865de938
kayla1410 <130771373+kayla1410@users.noreply.github.com>
2025-01-06 22:48:23 +0700

    Update pendidikan_microservices.sql

--- pendidikan_microservices.sql
+++ pendidikan_microservices.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2024 at 07:32 AM
+-- Generation Time: Jan 06, 2025 at 04:47 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -87,15 +87,20 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`no`, `id`, `nama`, `nim`, `semester`, `ip_sem1`, `ip_sem2`, `ip_sem3`, `ip_sem4`, `ip_sem5`, `ip_sem6`, `ip_sem7`, `ip_sem8`) VALUES
-(1, 1, 'kayla', 1, 4, 3.5, 3, 4, 0, 0, 0, 0, 0),
-(2, 2, 'monic', 2, 4, 3, 3.2, 3.5, 4, 0, 0, 0, 0),
-(3, 3, 'tara', 3, 2, 0, 0, 0, 0, 0, 0, 0, 0),
-(4, 4, 'opan', 4, 2, 0, 0, 0, 0, 0, 0, 0, 0),
-(6, 6, 'kadey', 6, 4, 0, 0, 0, 0, 0, 0, 0, 0),
-(7, 7, 'monica', 5, 5, 0, 0, 0, 0, 0, 0, 0, 0),
-(10, 10, 'kerlyn', 10, 6, 2, 2.2, 3, 4, 0, 0, 0, 0),
-(12, 33, 'kay', 33, 2, 0, 0, 0, 0, 0, 0, 0, 0),
-(16, 22, 'ojan', 22, 4, 0, 0, 0, 0, 0, 0, 0, 0);
+(1, 1, 'Kayla Dyara', 18222074, 6, 3.5, 3.7, 3.5, 3.5, 4, 0, 0, 0),
+(2, 2, 'Monica Angela Hartono', 18222078, 4, 3, 3.2, 3.5, 4, 0, 0, 0, 0),
+(3, 3, 'Tamara Mayranda Lubis', 18222026, 3, 3.4, 4, 0, 0, 0, 0, 0, 0),
+(10, 10, 'Keryn Deslia Andeskar', 18222090, 3, 3, 3.2, 0, 4, 0, 0, 0, 0);
+
+--
+-- Triggers `students`
+--
+DELIMITER $$
+CREATE TRIGGER `auto_fill_no` BEFORE INSERT ON `students` FOR EACH ROW BEGIN
+    SET NEW.no = NEW.id; -- Atau gunakan logika lain jika sebaliknya
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -125,7 +130,8 @@ INSERT INTO `students_courses` (`id`, `student_id`, `course_id`, `semester`, `gr
 (11, 10, 1, 4, ''),
 (12, 10, 3, 4, ''),
 (13, 10, 5, 4, ''),
-(14, 1, 2, 4, '');
+(14, 1, 2, 4, ''),
+(15, 1, 6, 4, '');
 
 -- --------------------------------------------------------
 
@@ -168,7 +174,6 @@ INSERT INTO `users` (`no`, `id`, `username`, `password`, `role`) VALUES
 (5, 2, 'admin', '$2y$10$Pn8VaKRjzFJbYKVPp0Ov3u0goxl6SJXDCMfqdoA3PHRH6nLwyk21a', 'admin'),
 (6, 1, 'kayla', '$2y$10$qJgi.kWuwB63LriWW6IV7.j90KcIg2SYON5aRK6u9qsIwmKRQ.ZDa', 'student'),
 (7, 2, 'monic', '$2y$10$LQ6GyfNka8pLizk0wyJjcucQtqhGqqpa9cXffP96gKns8DJVjRJ/i', 'student'),
-(8, 10, 'teacher', '$2y$10$Em1oR8BcU1iyuiWtkH8utuIcuBbwxAnr/1h0G3.LRrfViNXSb.tBG', 'teacher'),
 (9, 10, 'kerlyn', '$2y$10$vLcsjKjP8OK9eqtByAFciuDcRqTLIdXPBYeUjhDhbEWx2Nk4F.jwq', 'student');
 
 --
@@ -227,31 +232,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18222073;
 
 --
 -- AUTO_INCREMENT for table `students_courses`
 --
 ALTER TABLE `students_courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `teacher`
 --
 ALTER TABLE `teacher`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
